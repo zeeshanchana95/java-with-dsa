dfddc70e6c333d0d84fba83e2d48b704535f4e81
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2024-05-03 22:37:19 +0500

    calculate average using functions in java

--- src/Main.java
+++ src/Main.java
@@ -2,40 +2,20 @@ import java.util.Scanner;
 
 public class Main {
 
-//    public static int calculateSum(int a, int b) {
-//        return a + b;
-//    }
-//
-//    public static int calculateProduct(int a, int b) {
-//        return a * b;
-//    }
+    //1. Enter 3 numbers from the user & make a function to print their values.
 
-    public static void printFactorial(int n) {
-        //loop
-        if(n < 0) {
-            System.out.println("Invalid number");
-            return;
-        }
-        int factorial = 1;
-        for(int i = n; i>=1; i--) {
-            factorial = factorial * i;
-        }
-        System.out.println(factorial);
+    public static void printAverage(int a, int b, int c) {
+        float avg = (float) (a + b + c) / 3;
+        System.out.println("Average of 3 Numbers is : " + avg);
         return;
     }
+
     public static void main(String[] args) {
-        //functions
         Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
 
-//        int a = sc.nextInt();
-//        int b = sc.nextInt();
-//       int sum = calculateSum(a, b);
-//       System.out.println("Sum of 2 numbers is: " + sum);
-//
-//       System.out.println("Product of 2 numbers is: " + calculateProduct(a,b));
-
-        int n = sc.nextInt();
-        printFactorial(n);
-
+        printAverage(a, b, c);
     }
 }
