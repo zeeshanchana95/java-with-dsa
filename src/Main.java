c6fa3035c1fee650c506626e333d3245b2013885
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2024-05-03 15:41:38 +0500

    functions with  return type

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,18 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void printMyName(String name) {
-        System.out.println(name);
-        return;
+    public static int calculateSum(int a, int b) {
+        int sum = a + b;
+        return sum;
     }
 
     public static void main(String[] args) {
         //functions
         Scanner sc = new Scanner(System.in);
-        String name = sc.next();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
 
-        printMyName(name);
-
+       int sum = calculateSum(a, b);
+        System.out.println("Sum of 2 numbers is: " + sum);
     }
 }
