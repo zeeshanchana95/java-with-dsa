2ad6ecddfb459660f89a9d025854b28284e5151c
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2024-05-03 22:46:52 +0500

    calculate sum of odd numbers from 1 to n using functions in java

--- src/Main.java
+++ src/Main.java
@@ -10,12 +10,25 @@ public class Main {
         return;
     }
 
+    public static void printSumOfOddNumbers(int n) {
+        int sum = 0;
+        for(int i=1; i<=n; i++) {
+            if(i % 2 == 1) {
+                sum += i;
+            }
+        }
+        System.out.println("Sum of odd numbers from 1 to " + n + " is : " + sum);
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
 
-        printAverage(a, b, c);
+//        int a = sc.nextInt();
+//        int b = sc.nextInt();
+//        int c = sc.nextInt();
+//        printAverage(a, b, c);
+
+        int n = sc.nextInt();
+        printSumOfOddNumbers(n);
     }
 }
