3cd7e431b978df3303c8ef05687b1be613b583e6
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2024-05-03 20:33:44 +0500

    calculate factorial using functions

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,36 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static int calculateSum(int a, int b) {
-        int sum = a + b;
-        return sum;
-    }
+//    public static int calculateSum(int a, int b) {
+//        return a + b;
+//    }
+//
+//    public static int calculateProduct(int a, int b) {
+//        return a * b;
+//    }
 
+    public static void printFactorial(int n) {
+        //loop
+        int factorial = 1;
+        for(int i = n; i>=1; i--) {
+            factorial = factorial * i;
+        }
+        System.out.println(factorial);
+        return;
+    }
     public static void main(String[] args) {
         //functions
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
 
-       int sum = calculateSum(a, b);
-        System.out.println("Sum of 2 numbers is: " + sum);
+//        int a = sc.nextInt();
+//        int b = sc.nextInt();
+//       int sum = calculateSum(a, b);
+//       System.out.println("Sum of 2 numbers is: " + sum);
+//
+//       System.out.println("Product of 2 numbers is: " + calculateProduct(a,b));
+
+        int n = sc.nextInt();
+        printFactorial(n);
+
     }
 }
