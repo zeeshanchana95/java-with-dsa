87b2301e2dbdb24cc6fb9a605bb00f496f6790e2
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2024-05-03 13:13:16 +0500

    half pyramid using java

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         //pattern questions
-        int rows = 4;
-        int cols = 5;
-        for(int i = 1; i<= rows; i++) {
-            for(int j = 1; j <= cols; j++) {
+        int n = 4;
+        for(int i = 1; i<= n; i++) {
+            for(int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
             System.out.println();
