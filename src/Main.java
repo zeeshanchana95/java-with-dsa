11a4414fb83d683cb93658e86e9a3a96339573ef
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2024-05-03 15:37:47 +0500

    functions with void return type

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,18 @@
+import java.util.Scanner;
 
 public class Main {
+
+    public static void printMyName(String name) {
+        System.out.println(name);
+        return;
+    }
+
     public static void main(String[] args) {
-        //pattern questions
-        int n = 4;
-        for(int i = 1; i<=n; i++) {
-            //inner loop -> print space
-            for(int j = 1; j <=n-i; j++) {
-                System.out.print(" ");
-            }
-            //inner loop -> print *
-            for(int j=1; j<=i; j++) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
+        //functions
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+
+        printMyName(name);
 
     }
 }
