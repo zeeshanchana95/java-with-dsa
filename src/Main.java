fa07a2ead19ea10aa9657482b08562d906bae2b8
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2024-05-05 10:29:15 +0500

    practice problem on arrays in java

--- src/Main.java
+++ src/Main.java
@@ -3,28 +3,21 @@ import java.util.Scanner;
 public class Main {
 
 
+    //    Question: Take an array as input from the user. Search for a
+    //    given number x and print the index at which it occurs.
+
+    public static void getIndex(int[] arr, int num) {
+        for(int i=0; i<arr.length; i++){
+            if(arr[i] == num) {
+                System.out.println("Number found at index " + i);
+                return;
+            }
+        }
+        System.out.println("Number not found");
+    }
 
     public static void main(String[] args) {
-//        int[] marks = new int[3];
-//        int marks[] = {97, 98, 95};
-//        marks[0] = 97;
-//        marks[1] = 98;
-//        marks[2] = 95;
-//        System.out.println(marks[0]);
-//        System.out.println(marks[1]);
-//        System.out.println(marks[2]);
-        Scanner sc = new Scanner(System.in);
-        int size = sc.nextInt();
-        int numbers[] = new int[size];
-
-        // for input
-        for(int i=0; i<size; i++){
-            numbers[i] = sc.nextInt();
-        }
-
-        //for output
-        for (int i=0; i<size; i++){
-            System.out.println(numbers[i]);
-        }
+        int[] numbers = {1,2,3,4};
+        getIndex(numbers, 0);
     }
 }
