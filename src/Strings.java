ecf0f328d12b6997dd33dbe3af722568b52a7ed1
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2024-05-05 21:15:11 +0500

    StringBuilder in Java

--- src/Strings.java
+++ src/Strings.java
@@ -1,65 +1,36 @@
-import java.util.Scanner;
 
 public class Strings {
 
     public static void main(String[] args) {
-        //String functions
+        StringBuilder sb = new StringBuilder("Zeeshan");
+        System.out.println(sb);
 
-        //concatenation
-        String firstName = "Zeeshan";
-        String lastName = "Ahmed";
-        String fullName = firstName + " " + lastName;
-        System.out.println(fullName);
+        //char at index 0
+        System.out.println(sb.charAt(0));
+
+        //set char at index
+        sb.setCharAt(0, 'A');
+        System.out.println(sb);
+
+        //insert character at specific index and shift other characters
+        sb.insert(1, "hmed Z");
+        System.out.println(sb);
+
+        //delete specific part from string
+        sb.delete(0, 6);
+        System.out.println(sb);
+
+        //append other string
+        sb.append(" ");
+        sb.append("A");
+        sb.append("h");
+        sb.append("med");
+        System.out.println(sb);
 
         //length
-        System.out.println("Length : " + fullName.length());
+        System.out.println(sb.length());
 
-        //charAt
-        for(int i=0; i<fullName.length(); i++) {
-            System.out.print(fullName.charAt(i));
-        }
-        System.out.println();
 
-        //compareTo
-            //1. s1 > s2  -> +ve value
-            //2. s1 == s2 -> 0 value
-            //3. s1 < s2  -> -ve value
-
-        String name1 = "Zeeshan2";
-        String name2 = "Zeeshan";
-        if(name1.compareTo(name2) == 0) {
-            System.out.println("Strings are equal");
-        } else {
-            if(name1.compareTo(name2) > 0) {
-                System.out.println("String1 is greater than String2");
-            } else {
-                System.out.println("String1 is less than String2");
-            }
-        }
-
-        if(name1 == name2) {
-            System.out.println("Strings are equal");
-        } else {
-            System.out.println("Strings are not equal");
-        }
-
-        if(new String("Zeeshan") == new String("Zeeshan")) {
-            System.out.println("Equal");
-        } else {
-            System.out.println("Not equal");
-        }
-
-        //substring
-        String sentence = "My name is Zeeshan";
-        String newName = sentence.substring(11, sentence.length());
-        System.out.println(newName);
-
-        String newString = "ZeeshanAhmed";
-        String subStr = newString.substring(0, 7);
-        System.out.println(subStr);
-
-        String subStr1 = newString.substring(7);
-        System.out.println(subStr1);
 
     }
 }
