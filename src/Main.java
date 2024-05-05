da876a58107a19e21e95bfb4ac0be78f55f441d3
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2024-05-05 11:42:38 +0500

    2D arrays in Java

--- src/Main.java
+++ src/Main.java
@@ -3,21 +3,25 @@ import java.util.Scanner;
 public class Main {
 
 
-    //    Question: Take an array as input from the user. Search for a
-    //    given number x and print the index at which it occurs.
-
-    public static void getIndex(int[] arr, int num) {
-        for(int i=0; i<arr.length; i++){
-            if(arr[i] == num) {
-                System.out.println("Number found at index " + i);
-                return;
-            }
-        }
-        System.out.println("Number not found");
-    }
 
     public static void main(String[] args) {
-        int[] numbers = {1,2,3,4};
-        getIndex(numbers, 0);
+        Scanner sc = new Scanner(System.in);
+        int rows = sc.nextInt();
+        int cols = sc.nextInt();
+
+        int[][] numbers = new int[rows][cols];
+
+        for(int i=0; i<rows; i++) {
+            for(int j=0; j<cols; j++) {
+                numbers[i][j] = sc.nextInt();
+            }
+        }
+
+        for(int i=0; i<rows; i++) {
+            for(int j=0; j<cols; j++) {
+                System.out.print(numbers[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
