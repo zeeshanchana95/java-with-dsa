d2e437214a5f1c75a263e675a1423cf9fbfe27ec
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2024-05-08 08:35:14 +0500

    factorial using recursion in Java

--- src/RecursionDemo.java
+++ src/RecursionDemo.java
@@ -18,6 +18,14 @@ public class RecursionDemo {
         printNumbersInSequence(n+1);
     }
 
+    public static int calcFactorial(int n) {
+        if(n==1 | n==0)
+            return 1;
+        int fact_nm1 = calcFactorial(n-1);
+        int fact_n = n * fact_nm1;
+        return fact_n;
+    }
+
     public static void printSum(int i, int n, int sum) {
         if(i==n){
             sum +=i;
@@ -27,12 +35,17 @@ public class RecursionDemo {
         sum += i;
         printSum(i+1, n, sum);
     }
+
     public static void main(String[] args) {
 
 //        int n = 5;
 //        printNumbers(n);
 
-        int n = 1;
-        printNumbersInSequence(n);
+//        int n = 1;
+//        printNumbersInSequence(n);
+
+        int n = 5;
+        int ans = calcFactorial(n);
+        System.out.println(ans);
     }
 }
