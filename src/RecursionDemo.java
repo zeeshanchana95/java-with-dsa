5a68a79e1d24a1a3050ea8894a417a9eb701ec89
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2024-05-09 22:12:56 +0500

    print factorial using recursion in Java

--- src/RecursionDemo.java
+++ src/RecursionDemo.java
@@ -36,6 +36,17 @@ public class RecursionDemo {
         printSum(i+1, n, sum);
     }
 
+    //Q: Print fibonacci sequence till nth term.
+    public static void printFib(int a, int b, int n) {
+       if(n==0) {
+           return;
+       }
+       int c = a+b;
+       System.out.println(c);
+       printFib(b, c, n-1);
+    }
+
+
     public static void main(String[] args) {
 
 //        int n = 5;
@@ -44,8 +55,14 @@ public class RecursionDemo {
 //        int n = 1;
 //        printNumbersInSequence(n);
 
-        int n = 5;
-        int ans = calcFactorial(n);
-        System.out.println(ans);
+//        int n = 5;
+//        int ans = calcFactorial(n);
+//        System.out.println(ans);
+
+        int a = 0, b = 1;
+        System.out.println(a);
+        System.out.println(b);
+        int n = 7;
+        printFib(a, b, n-2);
     }
 }
